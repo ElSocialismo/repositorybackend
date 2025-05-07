7a00d7a181b952f3b5e814e65c062c43cc8c4461
ElSocialismo <adriano.alejo@tecsup.edu.pe>
2025-05-07 16:50:53 -0500

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+function requestController() {
+  console.log('Bienvenidos al curso');
+}
+
+const server = http.createServer(requestController);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, function() {
+  console.log("Aplicacion corriendo en: " + PORT);
+});
